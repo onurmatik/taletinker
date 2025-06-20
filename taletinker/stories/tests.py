c56933723be207ab586740f5e20d45c1757315df
onur mat <onurmatik@gmail.com>
2025-06-20 09:53:27 +0300

    Save the story

--- taletinker/stories/tests.py
+++ taletinker/stories/tests.py
@@ -1,10 +1,37 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from taletinker.accounts.models import User
+from taletinker.stories.models import Story
+
 
 class CreateStoryViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="test", password="pass")
+        self.client = Client()
+
     def test_get_create_page(self):
-        client = Client()
-        response = client.get(reverse("create_story"))
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("create_story"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Generate Story")
+
+    def test_post_creates_story(self):
+        self.client.force_login(self.user)
+        data = {
+            "realism": 50,
+            "didactic": 50,
+            "age": 5,
+            "themes": ["family"],
+            "purposes": ["joyful"],
+            "characters": "Jane",
+            "extra_instructions": "A test story.",
+            "story_length": "short",
+            "language": "en",
+        }
+        response = self.client.post(reverse("create_story"), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Story.objects.count(), 1)
+        story = Story.objects.first()
+        self.assertEqual(story.author, self.user)
+        self.assertEqual(story.parameters["realism"], 50)
--- taletinker/stories/views.py
+++ taletinker/stories/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
 from .forms import StoryCreationForm
+from .models import Story
 
 
+@login_required
 def create_story(request):
-    form = StoryCreationForm()
+    if request.method == "POST":
+        form = StoryCreationForm(request.POST)
+        if form.is_valid():
+            Story.objects.create(
+                author=request.user,
+                parameters={
+                    "realism": form.cleaned_data["realism"],
+                    "didactic": form.cleaned_data["didactic"],
+                    "age": form.cleaned_data["age"],
+                    "themes": form.cleaned_data["themes"],
+                    "purposes": form.cleaned_data["purposes"],
+                    "characters": form.cleaned_data["characters"],
+                    "story_length": form.cleaned_data["story_length"],
+                },
+                prompt=form.cleaned_data["extra_instructions"],
+                original_language=form.cleaned_data["language"],
+            )
+            return redirect("create_story")
+    else:
+        form = StoryCreationForm()
     return render(request, "stories/create_story.html", {"form": form})
