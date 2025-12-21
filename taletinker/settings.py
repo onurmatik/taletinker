7b6eac8d7336e84dbaa505409521e5abb4b946de
onur mat <onurmatik@gmail.com>
2025-12-21 19:12:28 +0300

    settings update

--- taletinker/settings.py
+++ taletinker/settings.py
@@ -11,7 +11,6 @@ load_dotenv()
 logger = logging.getLogger("taletinker.settings")
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
